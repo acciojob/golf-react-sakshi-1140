c0cd3308fa4182ba4a5b14ae1ab0e8f9c05d2676
Sakshi <sakshichoudhary000@gmail.com>
2024-07-25 18:53:48 +0530

    copied just to check test case

--- src/components/App.js
+++ src/components/App.js
@@ -11,22 +11,36 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.handleKeyDown=this.handleKeyDown.bind(this);
     };
 
     buttonClickHandler() {
-   
+        this.setState({ renderBall: true });
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
-
-    // bind ArrowRight keydown event
+    handleKeyDown(event) {
+        if (event.key === 'ArrowRight' || event.keyCode === 39) {
+            this.setState(prevState => {
+                const newPosi = prevState.posi + 5;
+                return {
+                    posi: newPosi,
+                    ballPosition: { left: `${newPosi}px` }
+                };
+            });
+        }
+    }
     componentDidMount() {
+        document.addEventListener('keydown',this.handleKeyDown);
       
+    }
+    componentWillUnmount(){
+        document.removeEventListener('keydown',this.handleKeyDown);
     }
 
     render() {
